65be75791eeb3de52b5c29db4777c370b766011a
Smokeekreek <164947621+Smokeekreek@users.noreply.github.com>
2024-12-26 20:02:57 -0500

    Initial commit

---
+++ src/main/java/net/smokeekreek/smokesfirstmod/SmokesFirstMod.java
@@ -0,0 +1,20 @@
+package net.smokeekreek.smokesfirstmod;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class SmokesFirstMod implements ModInitializer {
+	public static final String MOD_ID = "smokesfirstmod";
+
+	// This logger is used to write text to the console and the log file.
+	// It is considered best practice to use your mod id as the logger's name.
+	// That way, it's clear which mod wrote info, warnings, and errors.
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/net/smokeekreek/smokesfirstmod/SmokesFirstModClient.java
@@ -0,0 +1,10 @@
+package net.smokeekreek.smokesfirstmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class SmokesFirstModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
